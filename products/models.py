00d2c1d74eb65902846fa5cf0356e312b3bc03b0
muhammadsaim <muhammadsaim494@gmail.com>
2018-10-10 22:26:13 +0500

    add products list view and details view

--- products/models.py
+++ products/models.py
@@ -1,9 +1,23 @@
+import random,string,os
 from django.db import models
+
+def get_file_ext(filepath):
+    basename = os.path.basename(filepath)
+    name, ext = os.path.splitext(basename)
+    return name, ext
+
+def upload_image_path(instance, filename):
+    new_filename = ''.join([random.choice(string.ascii_lowercase) for i in range(16)])
+    name, ext = get_file_ext(filename)
+    final_filename = f'{new_filename}{ext}'
+    return f'products/{final_filename}'
 
 # Create your models here.
 class Product(models.Model):
     title           = models.CharField(max_length=225)
+    slug            = models.SlugField(unique=True)
     description     = models.TextField()
+    image           = models.ImageField(upload_to=upload_image_path)
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
 
     def __str__(self):
